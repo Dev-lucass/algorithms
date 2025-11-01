d86377c445d8744a82a6b209840611b61b533e77
Dev-lucass <lucasbitencourt959@gmail.com>
2025-11-01 14:41:42 -0300

    more algorithms

---
+++ src/RecursaoLetrasMaiusculas.java
@@ -0,0 +1,21 @@
+public class RecursaoLetrasMaiusculas {
+
+    public static void main(String[] args) {
+        int result = RecursaoLetrasMaiusculas.recursion(
+                "Amor Elefante GaTo uRso CasA"
+        );
+
+        System.out.println(result);
+    }
+
+    public static int recursion(String s) {
+
+        if (s.isBlank()) return 0;
+
+        if (Character.isUpperCase(s.charAt(0))) {
+            return 1 + recursion(s.substring(1));
+        }
+
+        return recursion(s.substring(1));
+    }
+}
