40450a76df42fbc61ff64e5dd49c1d990f4132f1
Dev-lucass <lucasbitencourt959@gmail.com>
2025-11-01 14:00:07 -0300

    more algorithms

---
+++ src/RecursaoEmprimirPalavras.java
@@ -0,0 +1,22 @@
+public class RecursaoEmprimirPalavras {
+
+    public static void main(String[] args) {
+        RecursaoEmprimirPalavras.recursion("amor elefante gato urso casa");
+    }
+
+    public static void recursion(String s) {
+
+        if (s.isBlank()) return;
+
+        int indexOf = s.indexOf(" ");
+
+        if (indexOf > 0) {
+            System.out.println(s.substring(0,indexOf));
+            recursion(s.substring(indexOf + 1));
+        } else {
+            System.out.println(s);
+        }
+
+    }
+
+}
